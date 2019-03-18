2ba18e680f20b57fb842ed1a89a454463edb951e
Fireng <reyftds321@mail.ru>
2019-03-18 14:40:15 +0400

    Templates

--- lesson_one/views.py
+++ lesson_one/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
 # Create your views here.
 
-def index(request, question_id):
-    return HttpResponse("hi %s." % question_id)
+def index(request):
+    return render(request, 'site/index.html', {})
 
